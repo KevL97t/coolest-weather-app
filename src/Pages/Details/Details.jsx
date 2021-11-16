d753ce64a557ddb3d2e8a762dd3453f3123a4f73
kevl97t <kalarboleda97@mail.com>
2021-11-15 19:07:28 -0600

    addded poweredby section in details page

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -17,6 +17,13 @@ function Details() {
             {loading && <Loader />}
             <DetailsCard />
             <Link className="go-back-button" to="/">Go back</Link>
+            <div className="poweredby-container">
+                <p className="poweredby-text" >Powered by:</p>
+                <img 
+                src="https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png" 
+                alt="open-weather-api-logo" 
+                className="poweredby-img" />
+            </div>
         </div>
     );
 }
